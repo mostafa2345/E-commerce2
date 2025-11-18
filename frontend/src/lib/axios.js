3d20e771acfec52e0cb4f16ecc4923a5a1ee760e
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-18 15:36:55 +0200

    fix:cors

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance=axios.create({
-    baseURL:import.meta.env.mode==='development'?'http://localhost:5000/api':'/api',
+    baseURL:import.meta.env.mode==='development'?'http://localhost:5000/api':`${import.meta.env.VITE_API_URL}/api`,
     withCredentials:true
 })
 
