1737eee203d743ebfdef9065d1d71658504eb089
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-18 15:05:17 +0200

    fix:cors

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,16 @@ import couponRoutes from '../backend/routes/coupon.route.js'
 import paymentRoutes from '../backend/routes/payment.route.js'
 import analyticsRoutes from '../backend/routes/analytic.route.js'
 import cookieParser from 'cookie-parser'
-import path from 'path'
+import cors from 'cors'
 import { log } from './utils/logger.js'
 dotenv.config()
 const app=express()
 
 const PORT=process.env.PORT||5000
-const __dirname=path.resolve()
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true
+}))
 app.use(express.json({ limit: "10mb" }))
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
@@ -23,12 +26,7 @@ app.use('/api/cart',cartRoutes)
 app.use('/api/coupons',couponRoutes)
 app.use('/api/payments',paymentRoutes)
 app.use('/api/analytics',analyticsRoutes)
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'frontend','dist')))
-    app.get('/*path',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'))
-    })
-}
+
 app.listen(PORT,()=>{
     log(`Server is running on port ${PORT}`)
     connectDB()
