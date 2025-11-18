b8f635d93ba8e6ebfb54a4cb29ec2fce651fdbec
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-18 15:42:48 +0200

    fix:cors

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
-import 'dotenv/config'
+
 import { log } from '../utils/logger.js'
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
     log(`MongoDB connected: ${conn.connection.host}`)
   } catch (error) {
     log(`MongoDB connection error: ${error.message}`)
