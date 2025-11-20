c45497ebcbbc5fc0fafd89cd4d954c68cbc26624
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-20 18:48:59 +0200

    try number1M

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,11 @@ app.use('/api/cart',cartRoutes)
 app.use('/api/coupons',couponRoutes)
 app.use('/api/payments',paymentRoutes)
 app.use('/api/analytics',analyticsRoutes)
+dbConnect();
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
-app.listen(PORT,()=>{
-    log(`Server is running on port ${PORT}`)
-    dbConnect()
-})
