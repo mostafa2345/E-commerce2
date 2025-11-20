3190dcdd6e36dfadec999a2b8ba1fd4df172ac97
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-20 19:35:02 +0200

    gooo

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.NODE_ENV === 'production' 
+    baseURL: import.meta.env.VITE_API_URL
         ? `${import.meta.env.VITE_API_URL}/api` 
         : 'http://localhost:5000/api',
     withCredentials: true
