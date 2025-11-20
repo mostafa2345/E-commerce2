ac3d3cc081ff0d81f125dd453d5439b016f89d57
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-20 19:26:15 +0200

    try number nice

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' 
-        ? `${process.env.VITE_API_URL}/api` 
+    baseURL: import.meta.env.NODE_ENV === 'production' 
+        ? `${import.meta.env.VITE_API_URL}/api` 
         : 'http://localhost:5000/api',
     withCredentials: true
 })
