e37ad7ccee2442f66cec506a9df548d32080b25e
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-20 19:53:57 +0200

    let see

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,9 @@ app.use('/api/coupons',couponRoutes)
 app.use('/api/payments',paymentRoutes)
 app.use('/api/analytics',analyticsRoutes)
 dbConnect();
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
+
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-}
+
 
